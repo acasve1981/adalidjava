3dd77449a3226df05eef7d1cfd84950ddb1f748f
Java15 <Java15@192.168.1.53>
2017-05-24 21:10:00 +0200

    version1

---
+++ salud/src/main/java/salud/Salud.java
@@ -0,0 +1,16 @@
+package salud;
+
+import edu.femxa.val.imc.IMC;
+import edu.femxa.val.imc.Persona;
+import edu.femxa.val.imc.TipoIMC;
+
+public class Salud {
+public static void main (String[] args){
+	Persona persona =null;
+	persona = new Persona (60, 1.50);
+	double imc_numerico = IMC.calcula(persona);
+		
+TipoIMC tipo = TipoIMC.traduceIMC(imc_numerico);
+System.out.println(tipo);
+}
+}
---
+++ salud/src/main/java/salud/Sexo.java
@@ -0,0 +1,20 @@
+package salud;
+
+public class Sexo {
+	
+	private String sexo;
+	
+	public Sexo(String sexo) {
+		this.sexo = sexo;
+}
+	public String getsexo() {
+		return sexo;
+	}
+	public void sexo(String sexo) {
+		this.sexo = sexo;
+	}
+	if (sexo=="M"){
+		
+}
+	
+}
